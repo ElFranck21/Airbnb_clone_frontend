e71c533c858fd571c92ca00a7f5ac2d1d53d0264
Jesus Trinidad <trinidadjesus117@gmail.com>
2025-04-08 13:58:01 -0600

    organizacion

--- app/components/modals/SignupModal.tsx
+++ app/components/modals/SignupModal.tsx
@@ -2,28 +2,65 @@
 
 import Modal from "./Modal";
 import { useState } from "react"; 
+import { useRouter } from "next/navigation";
 import CustomButton from "../forms/CustomButtom";
 import useSignupModal from "@/app/hooks/useSignupModal";
+import apiService from "@/app/services/apiService";
 
 const SignupModal = ()=>{
-    const SignupModal= useSignupModal()
+    const router= useRouter();
+    const SignupModal= useSignupModal();
+    const [email, setEmail]= useState('');
+    const [password1, setPassword1]= useState('');
+    const [password2, setPassword2]= useState('');
+    const [errors, setErrors]= useState<string[]>([]);
+
+
+    const submitSignup= async()=>{
+        const formData= {
+            name: 'hola',
+            email: email,
+            password1: password1,
+            password2: password2,
+        }
+        const response = await apiService.post('/api/auth/register/', JSON.stringify (formData))
+        if (response.access){
+            SignupModal.close();
+            router.push('/')
+        } else{
+            const tmpErrors: string[]= Object.values(response).map((error:any)=>{
+                return error;
+            })
+            setErrors(tmpErrors)
+        }
+    }
+    
 
     const content=(
         <>
             <h2 className="mb-6 text-2xl">
             </h2>
-            <form className="space-y-4">
-                <input placeholder="Your email" type="email" className="w-full h-[54px] px-4 boder border-gray-300 rounded-xl" />
-                <input placeholder="Your password" type="password" className="w-full h-[54px] px-4 boder border-gray-300 rounded-xl" />
-                <input placeholder="Repeate password" type="password" className="w-full h-[54px] px-4 boder border-gray-300 rounded-xl" />
+            <form 
+                action={submitSignup}
+                className="space-y-4">
+                <input onChange={(e) => setEmail(e.target.value)} placeholder="Your email" type="email" className="w-full h-[54px] px-4 boder border-gray-300 rounded-xl" />
+                <input onChange={(e) => setPassword1(e.target.value)} placeholder="Your password" type="password" className="w-full h-[54px] px-4 boder border-gray-300 rounded-xl" />
+                <input onChange={(e) => setPassword2(e.target.value)} placeholder="Repeate password" type="password" className="w-full h-[54px] px-4 boder border-gray-300 rounded-xl" />
                 
-                <div className="p-5 bg-airbnb text-white rounded-xl opacity-80">
-                    The error message
-                </div>
-
+                {errors.map((error, index)=> {
+                return(
+                    <div 
+                        key={`error_${index}`}
+                        className="p-5 bg-airbnb text-white rounded-xl opacity-80">
+                        {error}
+                    </div>
+                )
+            })}
+                
                 <CustomButton
-                    label="submit"
-                    onclick={() => console.log('hola')} className={""}                    />
+                    label="Submit"
+                    onclick={submitSignup} className={''}
+                />
             </form>
         </>
     )
