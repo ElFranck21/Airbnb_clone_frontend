48dbf74faab7c7b1623215d3233c7c69796b14ae
Jesus Trinidad <trinidadjesus117@gmail.com>
2025-04-10 17:28:01 -0600

    landlord page dynamic

--- app/components/properties/PropertyList.tsx
+++ app/components/properties/PropertyList.tsx
@@ -1,7 +1,7 @@
 'use client';
 
 import apiService from "@/app/services/apiService";
-import { useEffect, useState } from "react";
+import React, { useEffect, useState } from "react";
 import PropertyListItem from "./PropertyListItem";
 
 export type PropertyType = {
@@ -11,11 +11,24 @@ export type PropertyType = {
     image_url: string;
 }
 
-const PropertyList = ()=>{
+interface PropertyListProps{
+    landlord_id?: string | null;
+}
+
+const PropertyList: React.FC <PropertyListProps> = ({
+    landlord_id
+})=>{
     const [properties, setProperties] = useState<PropertyType[]>([]);
 
     const getProperties=async ()=> {
-        const tmpProperties = await apiService.get('/api/properties/')
+        let url= '/api/properties/';
+        
+        if (landlord_id){
+            url += `?landlord_id=${landlord_id}`
+        }
+
+
+        const tmpProperties = await apiService.get(url)
         
         setProperties(tmpProperties.data);
     };
