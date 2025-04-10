2eb52ce95defcf24f996735967945c59105b3d04
Jesus Trinidad <trinidadjesus117@gmail.com>
2025-04-09 19:58:35 -0600

    ResevationSideBar automatic

--- app/components/properties/ReservationSidebar.tsx
+++ app/components/properties/ReservationSidebar.tsx
@@ -1,18 +1,70 @@
+'use client';
+
+import { useState, useEffect } from "react";
+import {Range} from 'react-date-range';
+import apiService from "@/app/services/apiService";
+import useLoginModal from "@/app/hooks/useLoginModal";
+
 import React from "react";
+import { difference } from "next/dist/build/utils";
+import {differenceInDays, eachDayOfInterval} from "date-fns";
+
+
+const initialDateRange={
+    startDate:new Date(),
+    endDate:new Date(),
+    key:'selection',
+}
 
 export type Property= {
     id: string;
+    guests: number;
     price_per_night: number;
 }
 
 interface ReservationSidebarProps{
+    userId: string | null,
     property: Property
 }
 
 
 const ReservationSidebar: React.FC<ReservationSidebarProps>=({
-    property
+    property,
+    userId
 })=>{
+    const loginModal= useLoginModal();
+    const [fee, setFee]= useState<number>(0);
+    const [nights, setNights]= useState<number>(1);
+    const [totalPrice, setTotalPrice]= useState<number>(0);
+    const [dateRange, setDateRange]= useState<Range>(initialDateRange);
+    const [minDate, setMinDate]= useState<Date>(new Date());
+    const [guests, setGuests]= useState<string>('1');
+    const guestsRange=Array.from({length: property.guests}, (_,index)=> index +1)
+
+    useEffect(()=>{
+        if(dateRange.startDate && dateRange.endDate){
+            const dayCount = differenceInDays(
+                dateRange.endDate,
+                dateRange.startDate
+            );
+            if (dayCount && property.price_per_night){
+                const _fee = ((dayCount * property.price_per_night)/100)*5;
+                setFee(_fee);
+                setTotalPrice((dayCount*property.price_per_night)+ _fee);
+                setNights(dayCount);
+            }else{
+                const _fee = (property.price_per_night/100)*5;
+
+                setFee(_fee);
+                setTotalPrice(property.price_per_night + _fee);
+                setNights(1);
+            }
+        }
+    }, [dateRange])
+
+
+
+
     return(
         <aside className="mt-4 p-6 col-span-2 rounded-xl border-gray-300 shadow-xl">
             <h2 className="mb-5 text-2xl">
@@ -20,13 +72,14 @@ const ReservationSidebar: React.FC<ReservationSidebarProps>=({
             </h2>
             <div className="mb-6 p-3 border border-gray-400 rounded-xl">
                 <label className="mb-2 block font-bold text-xs">Guest</label>
-                <select className="w-full -ml-1 text-xm">
-                    <option>1</option>
-                    <option>2</option>
-                    <option>3</option>
-                    <option>4</option>
-                    <option>5</option>
-                    <option>6</option>
+                <select
+                    value={guests}
+                    onChange={(e) => setGuests(e.target.value)}
+                    className="w-full -ml-1 text-xm"
+                >
+                    {guestsRange.map(number =>(
+                        <option key={number} value={number}>{number}</option>
+                    ))}
                 </select>
             </div>
 
@@ -35,19 +88,19 @@ const ReservationSidebar: React.FC<ReservationSidebarProps>=({
             </div>
 
             <div className="mb-4 flex justify-between align-center">
-                <p> $200 * 4 nights</p>
-                <p>$800</p>
+                <p> ${property.price_per_night} * {nights} nights</p>
+                <p>${property.price_per_night * nights}</p>
             </div>
 
             <div className="mb-4 flex justify-between align-center">
                 <p> Djangobnb fee</p>
-                <p>$40</p>
+                <p>${fee}</p>
             </div>
             <hr />
 
             <div className="mt-4 flex justify-between align-center font-bold">
                 <p> Total</p>
-                <p>$840</p>
+                <p>${totalPrice}</p>
             </div>
         </aside>
         
--- app/properties/[id]/page.tsx
+++ app/properties/[id]/page.tsx
@@ -1,11 +1,12 @@
 import ReservationSidebar from "@/app/components/properties/ReservationSidebar";
 import Image from "next/image";
 import apiService from "@/app/services/apiService";
-
+import { getUserId } from "@/app/lib/actions";
 
 
 const PropertyDetailPage = async ({params}: {params: {id: string }})=>{
     const property = await apiService.get(`/api/properties/${params.id}`)
+    const userId= await getUserId();
 
     return(
         <main className="max-w-[1700px] mx-auto px-6 pb-6">
@@ -44,6 +45,7 @@ const PropertyDetailPage = async ({params}: {params: {id: string }})=>{
                 </div>
                     <ReservationSidebar
                         property={property}
+                        userId={userId}
                     
                     />
             </div>
